9d6ad86e4e0493f005037ea47719a4f25dd017c1
himaniguptaNU <gupta.him@northeastern.edu>
2025-01-28 19:14:13 -0500

    updating the controllers and app.js for

--- src/app.js
+++ src/app.js
@@ -7,4 +7,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', healthRoutes);
 
+app.use((err,req,res,next) =>{
+    if(err instanceof SyntaxError && err.status ===400 && 'body' in err){
+        console.error('Invalid json or Syntax not correct of json');
+        return res.status(400).send();
+    }
+});
+
 module.exports = app;
