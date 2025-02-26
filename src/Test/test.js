1a67bf834fa9f8aadcc7cfa6720e7bdf0bc12efb
himaniguptaNU <gupta.him@northeastern.edu>
2025-02-26 16:37:07 -0500

    Checking build6

--- src/Test/test.js
+++ src/Test/test.js
@@ -14,9 +14,7 @@ describe("Health Check API (/healthz)", () => {
         await sequelize.authenticate(); // Ensure DB is connected before tests
     });
 
-    afterAll(async () => {
-        await sequelize.close(); // Close DB connection after tests
-    });
+    
 
 
      //Test 1: Should return 200 OK when GET request is made without body/query params
@@ -75,5 +73,7 @@ describe("Health Check API (/healthz)", () => {
         const res = await request(app).get("/healthz");
         expect(res.status).toBe(503);
     }); */
-    
+    afterAll(async () => {
+        await sequelize.close(); // Close DB connection after tests
+    });
 });
